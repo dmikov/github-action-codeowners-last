0a32718286fd0c61f9114e8da4ab034811824dfb
Dmitriy Krasnikov <dmikov@gmail.com>
2020-12-15 05:09:53 -0500

    Testing payload

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
 
+const req = {required: true}
 const DIRECTORY_TO_TRACK = core.getInput('directory_to_track', req);
 const NUMBER_OF_CODE_OWNERS = core.getInput('number_of_code_owners', req);
 const FILE_PATH = path.join(DIRECTORY_TO_TRACK, 'CODEOWNERS');
 
 
-const fileStream = fs.createReadStream(FILE_PATH);
+// const fileStream = fs.createReadStream(FILE_PATH);
+
+try {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
